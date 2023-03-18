e089d95366c42dee3d9219d4aad1469702674c54
BorysPy <Boryspiasny@gmail.com>
2023-03-18 16:08:07 +0100

    Jira API available for test

---
+++ Integration_test_bench/Jira_issue_api.py
@@ -0,0 +1,15 @@
+class JiraIssueApi:
+   def __init__(self, auth):
+      self.auth = auth
+  
+   def create_jira_issue(self, url):
+      pass
+
+   def read_jira_issue(self, url):
+      pass
+
+   def update_jira_issue(self, url):
+      pass
+
+   def delete_jira_issue(self, url):
+      pass
