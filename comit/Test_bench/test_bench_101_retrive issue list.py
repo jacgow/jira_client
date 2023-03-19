9d46ebafd47c00c8a9f883a434165185def23448
BorysPy <Boryspiasny@gmail.com>
2023-03-19 14:48:46 +0100

    Działa komunikacja po api dla całego test bench - do zrobienia test bench 101 CLASS

--- comit/Test_bench/test_bench_101_retrive issue list.py
+++ comit/Test_bench/test_bench_101_retrive issue list.py
@@ -3,7 +3,7 @@ from requests.auth import HTTPBasicAuth
 import json
 
 project_name = "ITB"
-url = "https://doubledare.atlassian.net/rest/api/3/issue/createmeta"
+url = "https://doubledare.atlassian.net/rest/api/3/issue/createmeta/ITB/"
 auth = HTTPBasicAuth("boryspiasny@gmail.com", "ATATT3xFfGF0hyibnD_Kj1g0wRUdrQrJq0uKTkNVgrATNYaC5y-4CN_p0T7XmGjLcj9OHFXBxd7Xv3ipo56eu8C9q9qi7ceEiLOoTOgwDTeyyYt78UypNVydEzU1Z4zD3udI0JGSoDk1JC-CrJB9MmBIy3cP1FWyFm1SJAGpeBxcX-RP-gKKxjQ=72A1CF0C")
 headers = {
   "Accept": "application/json"
