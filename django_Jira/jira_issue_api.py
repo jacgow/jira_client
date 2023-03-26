f3a5fec666a97a60712f011878c21fdc785557e7
BorysPy <Boryspiasny@gmail.com>
2023-03-26 10:34:10 +0200

    update class

--- django_Jira/jira_issue_api.py
+++ django_Jira/jira_issue_api.py
@@ -3,10 +3,16 @@ import json
 
 class JiraIssueApi:
 #############################################################################
-    def __init__(self, auth):
+    def __init__(self, auth,jira_base_url):
       self.auth = auth
+      self.jira_base_url = jira_base_url
+ 
 #############################################################################
-    def create_jira_issue(self, url):
+    def create_jira_issue(self):
+
+        jira_api_version = 2
+        url = f'{self.jira_base_url}/{jira_api_version}/issue/'
+
         headers = {"Accept": "application/json"}
 
         payload = json.dumps(
@@ -25,31 +31,37 @@ class JiraIssueApi:
         }
         )
 
-        if response.status_code == 200:
-                result = requests.post(url,headers = headers,data = payload,auth = auth)
+       # response = dict { 'status_code': 200 }
+        if result.status_code == 200:
+                result = requests.post(url,headers = headers,data = payload,auth = self.auth)
                 return result
         else:
-                raise Exception()
+                raise Exception()  
+#############################################################################
+    def get_jira_issue(self, issue_id):
         
-#############################################################################
-    def get_jira_issue(self, url):
+        jira_api_version = 2
+        url = f'{self.jira_base_url}/{jira_api_version}/issue/{issue_id}'
+        
+        headers = {"Accept": "application/json"}
+
+        response = requests.request(
+            "GET",
+            url,
+            headers=headers,
+            auth=self.auth
+        )
      
-      headers = {"Accept": "application/json"}
-
-      response = requests.request(
-         "GET",
-         url,
-         headers=headers,
-         auth=self.auth
-      )
-
-      if response.status_code == 200:
-         result = json.dumps(json.loads(response.text), sort_keys=True, indent=4, separators=(",", ": "))
-         return result
-      else:
-         raise Exception()
+        if response.status_code == 200:
+            result = json.dumps(json.loads(response.text), sort_keys=True, indent=4, separators=(",", ": "))
+            return result
+        else:
+            raise Exception(response.text)
 #############################################################################
-    def update_jira_issue(self, url):
+    def update_jira_issue(self, issue_id):
+         
+        jira_api_version = 3
+        url = f'{self.jira_base_url}/{jira_api_version}/issue/{issue_id}'
 
         headers = { "Accept": "application/json", "Content-Type": "application/json"}
 
@@ -72,40 +84,42 @@ class JiraIssueApi:
     def delete_jira_issue(self, url):
         pass
 #############################################################################
-    def get_list_jira_issues(self, url):
+    def get_list_jira_issues(self, query):
+
+        jira_api_version = 2
+        url = f'{self.jira_base_url}{jira_api_version}/search'
+
         headers = {
-        "Accept": "application/json"
+            "Accept": "application/json"
         }
 
-        query = {
-        'jql': 'project = ITB',
-            }
-
+       
         response = requests.request(
         "GET",
         url,
         headers=headers,
         params=query,
-        auth=auth
+        auth=self.auth
         )
 
         if response.status_code == 200:
-            result = json.dumps(json.loads(response.text), sort_keys=True, indent=4, separators=(",", ": "))
+            result = json.dumps(
+                json.loads(response.text), sort_keys=True, indent=4, separators=(",", ": ")
+                )
             return result
         else:
-            raise Exception()
-        
-    def jira_url(self, url)
-
+            raise Exception(response.text)
+#############################################################################        
 #############################################################################
+
 #'get issue'
 #url = 'https://doubledare.atlassian.net/rest/api/2/issue/10040'
 
 #'post issue'
 #url = 'https://doubledare.atlassian.net/rest/api/2/issue'
 
+#'update issue'
+# url = 'https://doubledare.atlassian.net/rest/api/3/issue/10040'
+
 #'retrive issue'
 #url = 'https://doubledare.atlassian.net/rest/api/2/search'
-
-#'update issue'
-#url = 'https://doubledare.atlassian.net/rest/api/3/issue/10040'
